d12aa2687ae6f50d606efe2f6161edf866625f17
Nitesh Shah <riddhisiddhiganeshfoundation@gmail.com>
2025-07-08 00:06:34 +0530

    changes

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function Footer() {
   return (
     <Box
       sx={{
-        backgroundColor: 'white',
-        color: 'white',
+        backgroundColor: "white",
+        color: "white",
         p: 2,
-        mt: 'auto',
-        textAlign: 'center',
+        mt: "auto",
+        textAlign: "center",
       }}
       component="footer"
     >
-      <Typography color='black' variant="body2">
-        © {new Date().getFullYear()} Riddhi Siddhi Ganesh Foundation. All rights reserved.
+      <Typography color="black" variant="body2">
+        © {new Date().getFullYear()} Riddhi Siddhi Ganesh Foundation. All rights
+        reserved. Designed with ❤️ by RSG Developers
       </Typography>
     </Box>
   );
